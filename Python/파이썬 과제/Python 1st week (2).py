c9e7ef247846b59c241eff50cbdb2d45bf5cb109
sojeong123 <hellownewworld1367@gmail.com>
2023-03-27 19:23:58 +0900

    First Update for Python

---
+++ Python/파이썬 과제/Python 1st week (2).py
@@ -0,0 +1,114 @@
+# # # # # a = input("> 1번째 숫자 : ")
+# # # # # b = input("> 2번째 숫자 : ")
+# # # # # print()
+# # # # # print(("{} + {} = {}".format(output_a + output_b)))
+
+# # # # string = input("정수 N을 입력하세요.")
+# # # # print("", string %% 2)
+
+# # # N = int(input("정수를 입력해주세요."))
+
+# # # i = 0
+# # # for i in range(1, N+1, 1) :
+# # #     if 
+
+
+# # a = input("문자열 입력>")
+# # b = input("문자열 입력>")
+# # print(a,b)
+
+# # c = a
+# # a = b
+# # b = c
+
+# # print(a,b)
+# # 서로 다른 N개의 자연수의 합이 S
+# # 자연수 N의 최댓값은 얼마일까? (반복문 사용)
+
+# S = int(input("S 입력 : "))
+# i = 1
+# sum = 0
+# count = 0
+# while True :
+#     sum += i
+#     count += 1
+#     print(i, end = '')
+#     if sum == S:
+#         break
+#     i += 1
+
+# print()
+# print(count)
+
+# # # # # a = input("> 1번째 숫자 : ")
+# # # # # b = input("> 2번째 숫자 : ")
+# # # # # print()
+# # # # # print(("{} + {} = {}".format(output_a + output_b)))
+
+# # # # string = input("정수 N을 입력하세요.")
+# # # # print("", string %% 2)
+
+# # # N = int(input("정수를 입력해주세요."))
+
+# # # i = 0
+# # # for i in range(1, N+1, 1) :
+# # #     if 
+
+
+# # a = input("문자열 입력>")
+# # b = input("문자열 입력>")
+# # print(a,b)
+
+# # c = a
+# # a = b
+# # b = c
+
+# # print(a,b)
+# # 서로 다른 N개의 자연수의 합이 S
+# # 자연수 N의 최댓값은 얼마일까? (반복문 사용)
+
+# # S = int(input("S 입력 : "))
+# # i = 1
+# # sum = 0
+# # count = 0
+# # while True :
+# #     sum += i
+# #     count += 1
+# #     print(i, end = '')
+# #     if sum == S:
+# #         break
+# #     i += 1
+
+# # print()
+# # print(count)
+
+
+# S = int(input("S 입력 : "))
+# i = 1
+# sum = 0
+# count = 0
+# while True :
+#     sum += i
+#     count += 1
+#     print(i, end = '')
+#     if sum == S:
+#         break
+#     i += 1
+
+# print()
+# print(count)
+
+# print(a.lower())
+# print(a.strip())
+# print(a.split())
+
+#입력받기
+string = input ("입력> ")
+#공백제거
+a = string.replace(' ', '')
+#소문자로 통일
+b = a.lower()
+#회문판별
+string[-1]
+
+print("회문 : ", string)
