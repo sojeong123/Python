0d5937ae322a1240e732a756b18779fb841504d6
sojeong123 <hellownewworld1367@gmail.com>
2023-03-30 15:51:02 +0900

    과카몰라

---
+++ Python/파이썬 과제/Python 4th week.py
@@ -0,0 +1,65 @@
+# for i in range(3) :
+#     print("안녕하세요.")
+
+# for i in range(5):
+#    print("*"*i)
+
+# for i in range(1,31,1):
+#     if i % 3 !== 0:
+#         print(i)
+#     else:
+#         print("****")
+
+
+#내가 한 것
+
+# A = int(input("2이상의 숫자를 입력해주세요. : "))
+# answer = 1
+
+# for i in range(1,A+1,1):
+#     answer = answer * i
+
+# print(i*(i+1))
+
+
+# A = int(input("2이상의 숫자를 입력해주세요. : "))
+# answer = 1
+
+# for i in range(1,A+1,1):
+#     answer = answer * i
+
+# print(answer7)
+
+# sum = 0
+
+# for i in range(1,11):
+#     sum = sum + 1
+# print(sum)
+
+# sum = 0
+
+# for i in range(1000,2001):
+#     if i % 2 == 1:
+#         sum = sum + i
+
+# print(sum)
+
+
+# 내가 한 것
+# A = int(input("구구단 몇단을 계산할까요? : "))
+# dan = 1
+
+# for i in range(A+1,10,1):
+#      dan = dan * i
+
+# print("A * i = "A*i)
+
+#미완성 교수
+# A = int(input("구구단 몇단을 계산할까요? : "))
+
+# for i in range(1,10):
+#     print("%d * %d = %d" %(dan, i, dan*i))
+
+for i in range(3):
+    for j in range(2):
+       print(i,j)
